a3f6b6bf6c4dba7a2b8bcc3f2d46734c8c26148c
zhang0135789 <zz634682577@163.com>
2019-08-14 01:10:10 -0700

    init 匿名函数函数

---
+++ demo6-go语言函数/demo4-func.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/*
+	Go 语言函数作为实参
+*/
+
+type cb func(int) int
+
+func main() {
+	testCallBack(1, callBack)
+	testCallBack(2, func(x int) int {
+		fmt.Printf("我是回调，x：%d\n", x)
+		return x
+	})
+}
+
+func testCallBack(x int, f cb) {
+	f(x)
+}
+
+func callBack(x int) int {
+	fmt.Printf("我是回调，x：%d\n", x)
+	return x
+}
---
+++ demo6-go语言函数/demo5-func.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+/*
+	Go 语言函数闭包
+	Go 语言支持匿名函数，可作为闭包。匿名函数是一个"内联"语句或表达式。匿名函数的优越性在于可以直接使用函数内的变量，不必申明
+*/
+
+func main() {
+	/* nextNumber 为一个函数，函数 i 为 0 */
+	nextNumber := getSequence()
+
+	/* 调用 nextNumber 函数，i 变量自增 1 并返回 */
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+
+	/* 创建新的函数 nextNumber1，并查看结果 */
+	nextNumber1 := getSequence()
+	fmt.Println(nextNumber1())
+	fmt.Println(nextNumber1())
+}
+
+func getSequence() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}

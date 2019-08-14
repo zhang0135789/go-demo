2253fb2e626c6422d5b2fa0b134199034cdb5c57
zhang0135789 <zz634682577@163.com>
2019-08-13 18:37:18 -0700

    init 循环

--- demo3-go语言循环语句/demo1-goto.go
+++ demo3-go语言循环语句/demo1-goto.go
@@ -3,16 +3,47 @@ package main
 import "fmt"
 
 func main() {
-	/*
-	   goto label;
-	   ..
-	   .
-	   label: statement;
-	*/
 
+	//demo1_goto()
+
+	demo2_for()
+}
+
+/*
+for key, value := range oldMap {
+    newMap[key] = value
+}
+*/
+func demo2_for() {
+
+	var b int = 15
+	var a int = 1
+	numbers := [6]int{1, 2, 3, 5}
+	/* for 循环 */
+	for a := 0; a < 10; a++ {
+		a++
+		fmt.Printf("a = 的值为: %d\n", a)
+	}
+	fmt.Printf("%d\n", a)
+	for a < b {
+		a++
+		fmt.Printf("a 的值为: %d\n", a)
+	}
+	fmt.Printf("%d\n", a)
+	for i, x := range numbers {
+		fmt.Printf("第 %d 位 x 的值 = %d\n", i, x)
+	}
+}
+
+/*
+   goto label;
+   ..
+   .
+   label: statement;
+*/
+func demo1_goto() {
 	/* 定义局部变量 */
 	var a int = 10
-
 	/* 循环 */
 LOOP:
 	for a < 20 {
--- demo4-go语言条件语句/demo1.go
+++ demo4-go语言条件语句/demo1.go
@@ -37,7 +37,7 @@ func demo6() {
 		fmt.Printf("received ", i1, " from c1\n")
 	case c2 <- i2:
 		fmt.Printf("sent ", i2, " to c2\n")
-	case i3, ok := (<-c3): // same as: i3, ok := <-c3
+	case i3, ok := (<-c3): // same as: i3, ok := <-c3o
 		if ok {
 			fmt.Printf("received ", i3, " from c3\n")
 		} else {

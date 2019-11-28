4d13c3f7d42529f0b3bded1d499ea0f9c9f6daba
zhang0135789 <zz634682577@163.com>
2019-11-27 19:31:40 -0800

    init gointro pipeline 内部排序网络版

--- demo20-gointro/cmd/externalSort/sort.go
+++ demo20-gointro/cmd/externalSort/sort.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 /**
@@ -14,6 +15,23 @@ MergeN - 搭建归并节点组
 pipeline的搭建及运行  - CPU及县城数量的观测
 */
 func main() {
+	//demo1()
+
+	demo2()
+
+}
+
+func demo2() {
+	infilename := "large.in"
+	//outfilename := "large.out"
+	p := createNetworkPipeline(infilename, 800000000, 8)
+
+	writeToFile(p, "large2.out")
+
+	printFile("large2.out")
+}
+
+func demo1() {
 	infilename := "large.in"
 	outfilename := "large.out"
 	p := createPipeline(infilename, 800000000, 8)
@@ -84,10 +102,36 @@ func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
 	return pipeline.MergeN(sortResults...)
 }
 
-/**
-把pipel写入文件
-*/
+func createNetworkPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	pipeline.Init()
 
-/**
-读取文件
-*/
+	sortAddr := []string{}
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i*chunkSize), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+
+		addr := ":" + strconv.Itoa(7000+i)
+
+		pipeline.NetworkSink(addr, pipeline.InMemSort(source))
+
+		sortAddr = append(sortAddr, addr)
+
+	}
+
+	sortResults := []<-chan int{}
+
+	//链接 server
+	// 取出个节点的结果  进行合并
+	for _, addr := range sortAddr {
+		sortResults = append(sortResults, pipeline.NetworkSource(addr))
+	}
+
+	return pipeline.MergeN(sortResults...)
+}
